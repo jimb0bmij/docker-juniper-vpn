fe208350c05e98a47d50abfdd90b1c96cb182f1e
Jim McCann <mccann.jim@gmail.com>
2017-05-04 14:33:16 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,6 +3,6 @@ set -e
 set -x
 
 cd /root/juniper-vpn-py
-./juniper-vpn.py --host {{JUNIPER_HOST}} --username {{JUNIPER_USER}} --password {{JUNIPER_PASSWORD}} --stdin DSID=%DSID% openconnect --no-cert-check --juniper %HOST% --cookie-on-stdin &
+./juniper-vpn.py --config ~/cvpn.cfg &
 iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
 iptables -A FORWARD -i eth0 -j ACCEPT
