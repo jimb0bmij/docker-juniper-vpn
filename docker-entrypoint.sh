7b8d0c818f607eb503de8246ac5094ab977f2c92
Jim McCann <mccann.jim@gmail.com>
2017-05-04 12:54:59 -0400

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,20 +2,20 @@
 
 set -x
 
-if [ -z "$JUNIPER_HOST" ]; then
-	echo >&2 'error: missing JUNIPER_HOST environment variable'
-	exit 1
-fi
+#if [ -z "$JUNIPER_HOST" ]; then
+#	echo >&2 'error: missing JUNIPER_HOST environment variable'
+#	exit 1
+#fi
 
-if [ -z "$JUNIPER_USER" ]; then
-	echo >&2 'error: missing JUNIPER_USER environment variable'
-	exit 1
-fi
-
-if [ -z "$JUNIPER_PASSWORD" ]; then
-	echo >&2 'error: missing JUNIPER_PASSWORD environment variable'
-	exit 1
-fi
+#if [ -z "$JUNIPER_USER" ]; then
+#	echo >&2 'error: missing JUNIPER_USER environment variable'
+#	exit 1
+#fi
+#
+#if [ -z "$JUNIPER_PASSWORD" ]; then
+#	echo >&2 'error: missing JUNIPER_PASSWORD environment variable'
+#	exit 1
+#fi
 
 sed 's/{{JUNIPER_HOST}}/'"${JUNIPER_HOST}"'/' -i /root/startup.sh
 sed 's/{{JUNIPER_USER}}/'"${JUNIPER_USER}"'/' -i /root/startup.sh
